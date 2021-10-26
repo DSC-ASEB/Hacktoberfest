d3ecd0c829fc65fb3db33e9fef7851c8c73553cc
Dara-Ekanth <daraekanth3@gmail.com>
2021-10-26 12:38:06 +0530

    Python: A python script to find the topview of a given Binary Search Tree.

---
+++ Python/Topview of a graph.py
@@ -0,0 +1,42 @@
+#first we have to build binary search tree.
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+def buildtree(root,data):
+    if root == None:
+        root = Node(data)
+    elif data < root.data:
+        root.left = buildtree(root.left,data)
+    elif data > root.data:
+        root.right = buildtree(root.right,data)
+    return root
+def inorder(root):
+    if root:
+        inorder(root.left)
+        print(root.data,end=" ")
+        inorder(root.right)
+
+def topview(root):    # For topview we require queue and hashmap
+    queue = []
+    queue.append([root,0])
+    hd = {}
+    while queue:
+        ele,idx = queue.pop(0)
+        if idx not in hd and ele:
+            hd[idx] = ele.data
+        if ele:
+            queue.append([ele.left,idx-1])
+            queue.append([ele.right,idx+1])
+    hd = dict(sorted(hd.items(),key=lambda x:x[1]))
+    print(*list(hd.values()),end=" ")
+
+
+if __name__ == "__main__":
+    a = list(map(int,input("Enter a list: ").split()))
+    root = None
+    for i in a:
+        root=buildtree(root,i)
+    topview(root)
