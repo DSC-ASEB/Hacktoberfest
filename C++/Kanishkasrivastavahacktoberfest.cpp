fe41675617c42efd6f1db9a8f7275fc337b01e2c
kanishkasrivastava <73534661+kanishkasrivastava@users.noreply.github.com>
2021-10-31 08:51:23 +0000

    Update Kanishkasrivastavahacktoberfest.cpp
    sir review #26  and #27 solved

--- C++/Kanishkasrivastavahacktoberfest.cpp
+++ C++/Kanishkasrivastavahacktoberfest.cpp
@@ -1,73 +1,89 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-class Box
+
+//Implement the class Box  
+class Box 
 {
     private:
-        int l,b,h;
-    
+    //l,b,h are integers representing the dimensions of the box
+    int length, breadth, height;
     public:
-        Box()
+    // Constructors: 
+    // Box();
+    Box() 
+    {
+        length = 0;
+        breadth = 0;
+        height = 0;
+    }
+    // Box(int,int,int);
+    Box(int l, int b, int h) 
+    {
+        length = l;
+        breadth = b;
+        height = h;
+    }
+    // Box(Box);
+    Box(const Box &B) 
+    {
+        length = B.length;
+        breadth = B.breadth;
+        height = B.height;
+    }
+    // int getLength(); // Return box's length
+    int getLength() 
+    {
+        return length;
+    }
+    // int getBreadth (); // Return box's breadth
+    int getBreadth() 
+    {
+        return breadth;
+    }
+    // int getHeight ();  //Return box's height
+    int getHeight() 
+    {
+        return height;
+    }
+    // long long CalculateVolume(); // Return the volume of the box
+    long long CalculateVolume() 
+    {
+        return (long long)(breadth) * length * height;
+    }
+    //Overload operator < as specified
+    //bool operator<(Box& b)
+    bool operator<(Box &B) 
+    {
+        if (length < B.length) 
         {
-            l = b = h = 0;
-        }
-        Box(int length,int breadth,int height)
+            return true;
+        } 
+        else if (length == B.length) 
         {
-            l = length;
-            b = breadth;
-            h = height;
-        }
-        Box(const Box& B)
-        {
-            l = B.l;
-            b = B.b;
-            h = B.h;
+            if (breadth < B.breadth) 
+            {
+                return true;
+            } 
+            else if (breadth == B.breadth) 
+            {
+                if (height < B.height) 
+                {
+                return true;
+                }
+            }
         }
 
-        int getLength()
-        {
-            return(l);
-        }
-        int getBreadth()
-        {
-            return(b);
-        }
-        int getHeight()
-        {
-            return(h);
-        }
-
-        long long CalculateVolume()
-        {
-            return ((long long)l*b*h);
-        }
-
-        friend bool operator < (Box& b1, Box& b2)
-        {
-            if((b1.l < b2.l) || (b1.l == b2.l && b1.b < b2.b) ||
-               (b1.l == b2.l && b1.b == b2.b && b1.h <b2.h))
-                return(true);
-            else
-                return(false);
-        }
-
-        friend ostream& operator << (ostream& s,Box& b1)
-        {
-            s << b1.l << " " << b1.b << " " << b1.h;
-            return s;
-        }
+        return false;
+    }
 };
-// int getLength(); // Return box's length
-// int getBreadth (); // Return box's breadth
-// int getHeight ();  //Return box's height
-// long long CalculateVolume(); // Return the volume of the box
-
-//Overload operator < as specified
-//bool operator<(Box& b)
-
 //Overload operator << as specified
 //ostream& operator<<(ostream& out, Box& B)
-
+ostream &operator<<(ostream &out, Box &B) 
+{
+    out << B.getLength() << " " << B.getBreadth() << " " << B.getHeight();
+    return out;
+};
 
 void check2()
 {
